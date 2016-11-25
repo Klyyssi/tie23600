6b0b3443007ce750198bc2ddeb04530359f9c59d
Markus Mulkahainen <markus.mulkahainen@hotmail.com>
2016-11-25 12:05:50 +0200

    adds initial project

---
+++ src/main/java/fi/group6/SpotchartApplication.java
@@ -0,0 +1,12 @@
+package fi.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotchartApplication.class, args);
+	}
+}
