3d8172a2df423a395ba009610206deb5a15a962e
Sampo Tolvanen <sampo.tolvanen@gmail.com>
2016-12-04 17:45:00 +0200

    Fix frontend API path

--- spotchart-frontend/src/config.js
+++ spotchart-frontend/src/config.js
@@ -1,9 +1,7 @@
 function generateApiUri(config) {
-  return config.hostname + ":" + config.port + config.prefix;
+  return config.prefix;
 }
 
 export const apiURI = generateApiUri({
-  hostname: "http://localhost",
-  port: 8080,
   prefix: "/api/v1"
 })
